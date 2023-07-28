7cbf8e079239119d30ac8a4150ccb5ece40692a6
hojun choi <aa8088cp4015timo12>
2023-07-28 23:49:13 +0900

    Initial Commit

---
+++ CapstoneProject Watch App/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject Watch App
+//
+//  Created by KNUSW_2 on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProject_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
